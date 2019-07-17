6b76950c5b03d56501eda0d41ce0831eeca1804c
unknown <yas.azi.ya@gmail.com>
2019-07-17 10:29:49 -0500

    First commit

---
+++ myScript.js
@@ -0,0 +1,50 @@
+// const name = "Yasmine";
+// var walls;
+// walls = "Great Walls";
+// function timesThree(num){
+//   return num * 3;
+// };
+//
+// function timesFive(num) {return num * 5};
+//
+// function helloworld() {alert("Hello World")};
+//
+// if (5 == 5) {
+//   alert("Yay!");
+// };
+// var number = -25;
+//
+// var msg = "Error";
+// var letter = 'm';
+// var age = 16
+// age = age + 1
+// var distance = 5.2;
+// distance = "Arrived";
+// var answer = "Yes";
+// answer = 'y';
+// let text = "";
+// for (var i = 0; i < 5; i ++) {
+//   text += " Num: " + i;
+// }
+
+// let num = 0
+//
+// for (var i = 0; i < 10; i++) {
+//   num += 1
+// }
+ function showTime() {
+   document.getElementById('demo').innerHTML=Date();
+ };
+
+alert("This website contains feminism and harry potter memes. You have been warned.")
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";}
+    else{
+      x.style.display = "none"
+    }
+  }
+}
